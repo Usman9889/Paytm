2d2af320959b5f600f2188b7cb033556aa079ae1
Usman9889 <usman9889a@gmail.com>
2026-06-10 15:20:36 +0530

    add_balance

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
