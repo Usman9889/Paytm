acaad67132adfdb1ee038f95ff4bd759ec7cbe61
Usman9889 <usman9889a@gmail.com>
2026-06-20 16:54:45 +0530

    Appbar

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -3,20 +3,22 @@ import { Button } from "./button";
 interface AppbarProps {
     user?: {
         name?: string | null;
+        // email?: string | null;
     },
     
     onSignin: any,
     onSignout: any
 }
 
+
 export const Appbar = ({
     user,
     onSignin,
     onSignout
-}: AppbarProps) => {
+}: AppbarProps) => {console.log("Appbar User:", user);
     return <div className="flex justify-between border-b px-4">
         <div className="text-lg flex flex-col justify-center">
-            PayTM
+            PayTM 
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
