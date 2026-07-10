d72ed737ad9fcf09f50689aafe89a75e98b59c1a
Usman9889 <usman9889a@gmail.com>
2026-07-10 20:22:06 +0530

    rm

--- apps/web/app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "../page.module.css";
-import { prisma } from "@repo/db";
-
-export default async function User() {
-  const user = await prisma.user.findFirst();
-  return <div className={styles.page}>{user?.name ?? "No user added yet"}</div>;
-}
